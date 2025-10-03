b409b97a201d89a8575b99524b7f8e47c121f43e
SigmaWater <artemudalcov05@gmail.com>
2025-10-03 21:46:42 +0300

    created migrations

---
+++ postgres/migrations/20251003213137_create_note_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+create table note(
+    id serial primary key,
+    title text not null,
+    body test not null,
+    create_at timestamp not null default now(),
+    update_at timestamp
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table note;
+-- +goose StatementEnd
