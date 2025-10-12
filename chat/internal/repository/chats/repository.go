5f305c6bbc6420df1cd1cf173c522608491ba246
SigmaWater <artemudalcov05@gmail.com>
2025-10-12 20:59:15 +0300

    fix create chat 3

--- chat/internal/repository/chats/repository.go
+++ chat/internal/repository/chats/repository.go
@@ -1,13 +1,14 @@
-package chats 
+package chats
 
-import(
-	"github.com/SigmarWater/messenger/chat/internal/model"
+import (
 	"context"
-	sq "github.com/Masterminds/squirrel"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/SigmarWater/messenger/chat/internal/model"
 	"github.com/SigmarWater/messenger/chat/internal/repository/chats/converter"
 	repoModel "github.com/SigmarWater/messenger/chat/internal/repository/chats/model"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type PostgresChatRepository struct {
@@ -15,21 +16,20 @@ type PostgresChatRepository struct {
 }
 
 func NewPostgresChatRepository(pool *pgxpool.Pool) *PostgresChatRepository {
-	return &PostgresChatRepository{pool:pool}
+	return &PostgresChatRepository{pool: pool}
 }
-
 
 // DeleteChat(ctx context.Context, idChat int)
 
-func (p *PostgresChatRepository) CreateChat(ctx context.Context, chatInfo *model.ChatService)(*model.ChatService, error){
+func (p *PostgresChatRepository) CreateChat(ctx context.Context, chatInfo *model.ChatService) (*model.ChatService, error) {
 	builderInsert := sq.Insert("chats").
-	PlaceholderFormat(sq.Dollar).
-	Columns("chat_name").
-	Values(chatInfo.ChatName).
-	Suffix("RETURNING id_chat")
+		PlaceholderFormat(sq.Dollar).
+		Columns("chat_name").
+		Values(chatInfo.ChatName).
+		Suffix("RETURNING id_chat, chat_name")
 
 	query, args, err := builderInsert.ToSql()
-	if err != nil{
+	if err != nil {
 		log.Printf("Ошибка при создании запроса в CreateChat: %v\n", err)
 		return nil, err
 	}
@@ -37,8 +37,8 @@ func (p *PostgresChatRepository) CreateChat(ctx context.Context, chatInfo *model
 	log.Printf("query: %v args: %v", query, args)
 
 	var chatRep repoModel.ChatRepository
-	err = p.pool.QueryRow(ctx, query, args).Scan(&chatRep.IdChat)
-	if err != nil{
+	err = p.pool.QueryRow(ctx, query, args).Scan(&chatRep.IdChat, &chatRep.ChatName)
+	if err != nil {
 		log.Printf("Ошибка выполнения запроса в CreateChat: %v\n", err)
 		return nil, err
 	}
@@ -46,17 +46,16 @@ func (p *PostgresChatRepository) CreateChat(ctx context.Context, chatInfo *model
 	return converter.MsgRepoToService(chatRep), nil
 }
 
-
-func (p *PostgresChatRepository) DeleteChat(ctx context.Context, idChat int) error{
+func (p *PostgresChatRepository) DeleteChat(ctx context.Context, idChat int) error {
 	builderDelete := sq.Delete("chats").PlaceholderFormat(sq.Dollar).Where(sq.Eq{"id_chat": idChat})
 
 	query, args, err := builderDelete.ToSql()
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	
+
 	_, err = p.pool.Exec(ctx, query, args)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
