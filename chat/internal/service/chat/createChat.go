77f7d244e165e9b6ab258f71fe7fefc477b15154
SigmaWater <artemudalcov05@gmail.com>
2025-10-12 20:29:00 +0300

    fix create(chat)

--- chat/internal/service/chat/createChat.go
+++ chat/internal/service/chat/createChat.go
@@ -3,12 +3,14 @@ package chat
 import(
 	"github.com/SigmarWater/messenger/chat/internal/model"
 	"context"
+	"log"
 )
 
 
 func (s *servChat) CreateChat(ctx context.Context, chatInfo *model.ChatService) (*model.ChatService, error){
 	chat, err := s.chatRepo.CreateChat(ctx, chatInfo)
 	if err != nil{
+		log.Printf("Ошибка в CreateChat: %v\n", err)
 		return nil, err
 	}
 	return chat, nil
