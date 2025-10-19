ec7aa3d6c113e2d4850b3795e7b026cfe62c6c83
SigmaWater <artemudalcov05@gmail.com>
2025-10-19 12:43:45 +0300

    fix: ports service

--- auth/internal/config/env/grpc.go
+++ auth/internal/config/env/grpc.go
@@ -1,33 +1,32 @@
-package env 
+package env
 
-import(
-	"os"
-	"net"
-	"github.com/SigmarWater/messenger/auth/internal/config"
+import (
 	"errors"
+	"github.com/SigmarWater/messenger/auth/internal/config"
+	"net"
+	"os"
 )
 
 var _ config.GRPCConfig = (*grpcConfig)(nil)
 
-
-const(
+const (
 	grpcEnvHost = "GRPC_HOST"
-	grpcEnvPort = "GRPC_PORT"
+	grpcEnvPort = "GRPC_PORT_AUTH"
 )
 
-type grpcConfig struct{
+type grpcConfig struct {
 	host string
-	port string  
+	port string
 }
 
-func NewGrpcConfig() (*grpcConfig, error){
+func NewGrpcConfig() (*grpcConfig, error) {
 	host := os.Getenv(grpcEnvHost)
-	if len(host) == 0{
+	if len(host) == 0 {
 		return nil, errors.New("not found host key")
 	}
 
 	port := os.Getenv(grpcEnvPort)
-	if len(port) == 0{
+	if len(port) == 0 {
 		return nil, errors.New("not found port key")
 	}
 
@@ -37,6 +36,6 @@ func NewGrpcConfig() (*grpcConfig, error){
 	}, nil
 }
 
-func (c *grpcConfig) Address() string{
+func (c *grpcConfig) Address() string {
 	return net.JoinHostPort(c.host, c.port)
 }
--- chat/internal/config/env/grpc.go
+++ chat/internal/config/env/grpc.go
@@ -4,39 +4,39 @@ import (
 	"net"
 	"os"
 
-	"github.com/SigmarWater/messenger/chat/internal/config"
 	"errors"
+	"github.com/SigmarWater/messenger/chat/internal/config"
 )
 
 var _ config.GRPCConfig = (*grpcConfig)(nil)
 
-const(
+const (
 	grpcHostEnvName = "GRPC_HOST"
-	grpcPortEnvName = "GRPC_PORT"
+	grpcPortEnvName = "GRPC_PORT_CHAT"
 )
 
-type grpcConfig struct{
-	host string 
-	port string 
+type grpcConfig struct {
+	host string
+	port string
 }
 
-func NewConfig() (*grpcConfig, error){
+func NewConfig() (*grpcConfig, error) {
 	host := os.Getenv(grpcHostEnvName)
-	if len(host) == 0{
+	if len(host) == 0 {
 		return nil, errors.New("grpc host not found")
 	}
 
 	port := os.Getenv(grpcPortEnvName)
-	if len(port) == 0{
+	if len(port) == 0 {
 		return nil, errors.New("grpc port not found")
 	}
 
 	return &grpcConfig{
 		host: host,
 		port: port,
-	}, nil 
+	}, nil
 }
 
-func (cfg *grpcConfig) Address() string{
+func (cfg *grpcConfig) Address() string {
 	return net.JoinHostPort(cfg.host, cfg.port)
 }
