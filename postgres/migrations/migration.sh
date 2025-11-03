6d85823a7c8b82db644c1f385b6ab4454c12f36e
SigmaWater <artemudalcov05@gmail.com>
2025-11-03 20:47:35 +0300

    test(2) docker file

--- postgres/migrations/migration.sh
+++ postgres/migrations/migration.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source .env
 
+DB_HOST=${DB_HOST:-postgres-prod}
+PG_PROD_PORT=${PG_PROD_PORT:-5432}
+MIGRATION_DIR=${MIGRATION_DIR:-/root/migrations}
+
 export MIGRATION_DSN="host=$DB_HOST port=$PG_PROD_PORT dbname=$PG_DATABASE_NAME user=$PG_USER password=$PG_PASSWORD sslmode=disable"
 
 sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
