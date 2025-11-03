21240afef300b286222ff0e5f284e305894bbd3e
SigmaWater <artemudalcov05@gmail.com>
2025-11-04 00:07:06 +0300

    added http

--- auth/internal/app/app.go
+++ auth/internal/app/app.go
@@ -7,13 +7,13 @@ import (
 	"sync"
 
 
-	//"net/http"
+	"net/http"
 	
 
 	"github.com/SigmarWater/messenger/auth/internal/closer"
 	"github.com/SigmarWater/messenger/auth/internal/config"
 	pb "github.com/SigmarWater/messenger/auth/pkg/api/auth_service"
-	// "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
@@ -23,7 +23,7 @@ import (
 type app struct {
 	serviceProvider *serviceProvider
 	grpcServer      *grpc.Server
-	//httpServer *http.Server 
+	httpServer *http.Server 
 }
 
 func NewApp(ctx context.Context) (*app, error) {
@@ -45,7 +45,7 @@ func (a *app) Run() error {
 
 	wg := &sync.WaitGroup{} 
 
-	wg.Add(1)
+	wg.Add(2)
 
 	go func(){
 		defer wg.Done() 
@@ -57,15 +57,15 @@ func (a *app) Run() error {
 		}
 	}()
 
-	// go func(){
-	// 	defer wg.Done() 
+	go func(){
+		defer wg.Done() 
 
-	// 	err := a.runHTTPServer()
+		err := a.runHTTPServer()
 
-	// 	if err != nil{
-	// 		log.Fatalf("failed to run GRPC server")
-	// 	}
-	// }()
+		if err != nil{
+			log.Fatalf("failed to run GRPC server")
+		}
+	}()
 
 	wg.Wait() 
 
@@ -78,7 +78,7 @@ func (a *app) initDeps(ctx context.Context) error {
 		a.initConfig,
 		a.initServiceProvider,
 		a.initGRPCServer,
-		//a.initHTTPServer,
+		a.initHTTPServer,
 	}
 
 	for _, f := range inits {
@@ -116,33 +116,33 @@ func (a *app) initGRPCServer(ctx context.Context) error {
 	return nil
 }
 
-// func (a *app) initHTTPServer(ctx context.Context) error{
-// 	// Создаем мультиплексор для HTTP запросов
-// 	mux := runtime.NewServeMux()
+func (a *app) initHTTPServer(ctx context.Context) error{
+	// Создаем мультиплексор для HTTP запросов
+	mux := runtime.NewServeMux()
 
-// 	// Настраиваем опции для соединения с gRPC сервером
-// 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	// Настраиваем опции для соединения с gRPC сервером
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 
-// 	// Регистрируем gRPC-gateway хендлеры
-// 	err := pb.RegisterUserAPIHandlerFromEndpoint(
-// 		ctx,
-// 		mux,
-// 		a.serviceProvider.grpcConfig.Address(),
-// 		opts,
-// 	)
+	// Регистрируем gRPC-gateway хендлеры
+	err := pb.RegisterUserAPIHandlerFromEndpoint(
+		ctx,
+		mux,
+		a.serviceProvider.grpcConfig.Address(),
+		opts,
+	)
 
-// 	if err != nil {
-// 		log.Printf("Failed to register gateway: %v\n", err)
-// 		return err 
-// 	}
+	if err != nil {
+		log.Printf("Failed to register gateway: %v\n", err)
+		return err 
+	}
 
-// 	a.httpServer = &http.Server{
-// 		Addr: a.serviceProvider.HTTPConfig().Address() ,
-// 		Handler: mux,
-// 	}
+	a.httpServer = &http.Server{
+		Addr: a.serviceProvider.HTTPConfig().Address() ,
+		Handler: mux,
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
 func (a *app) runGRPCServer() error {
 	log.Printf("GRPC server is running on %s", a.serviceProvider.GRPCConfig().Address())
@@ -160,14 +160,14 @@ func (a *app) runGRPCServer() error {
 	return nil
 }
 
-// func (a *app) runHTTPServer() error{
-// 	log.Printf("HTTP server is running on %s", a.serviceProvider.httpConfig.Address())
+func (a *app) runHTTPServer() error{
+	log.Printf("HTTP server is running on %s", a.serviceProvider.httpConfig.Address())
 
-// 	err := a.httpServer.ListenAndServe()
+	err := a.httpServer.ListenAndServe()
 
-// 	if err != nil{
-// 		return err 
-// 	}
+	if err != nil{
+		return err 
+	}
 
-// 	return nil
-// }
+	return nil
+}
